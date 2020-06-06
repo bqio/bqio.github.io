5dd0b39fbcbb08c525d1073ccd1636a0eb6c891e
Dmitry Mikhaltsov <45086516+bqio@users.noreply.github.com>
2020-06-06 10:46:19 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,47 +2,35 @@
 // 2020
 
 const canvas = document.getElementById("canvas");
+const menuContainer = document.getElementById("menuContainer");
+const btnRotate = document.getElementById("btnRotate");
+const btnScale = document.getElementById("btnScale");
+const btnReflection = document.getElementById("btnReflection");
 const ctx = canvas.getContext("2d");
 
-let X_POS = 100;
-let Y_POS = 125;
+let X_POS = 0;
+let Y_POS = 0;
 
+let isScale = false;
+let isRef = false;
 let scale = 1;
+let rotate = 0;
+let ref = [-1, 1];
 
-// Rotate method
-document.getElementById("rotate").addEventListener("click", () => {
-  ctx.translate(X_POS, Y_POS);
-  ctx.rotate((Math.PI / 180) * 10);
-  ctx.translate(-X_POS, -Y_POS);
+function mouseMoveHandler(evt) {
+  const { x, y } = getMousePos(canvas, evt);
+  X_POS = x;
+  Y_POS = y;
+}
+function contextMenuHandler(evt) {
+  evt.preventDefault();
+  canvas.removeEventListener("mousemove", mouseMoveHandler);
+  document.addEventListener("keyup", keyupHandler);
+  menuContainer.classList.remove("hidden");
   render();
-});
-
-// Reflection method
-document.getElementById("reflection").addEventListener("click", () => {
-  ctx.translate(X_POS, Y_POS);
-  ctx.scale(-1, 1);
-  ctx.translate(-X_POS, -Y_POS);
-  render();
-});
-
-// Scale method
-document.getElementById("scale").addEventListener("click", () => {
-  if (scale == 1.3) {
-    scale = 1;
-    ctx.restore();
-  } else {
-    scale = 1.3;
-  }
-  ctx.save();
-  ctx.translate(X_POS, Y_POS);
-  ctx.scale(scale, scale);
-  ctx.translate(-X_POS, -Y_POS);
-  render();
-});
-
-// Move method
-document.addEventListener("keyup", (e) => {
-  switch (e.key) {
+}
+function keyupHandler(evt) {
+  switch (evt.key) {
     case "ArrowLeft":
       X_POS -= 5;
       break;
@@ -57,37 +45,70 @@ document.addEventListener("keyup", (e) => {
       break;
   }
   render();
-});
-
-render();
-
-function render() {
-  // Clear canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-
+}
+function rotateHandler() {
+  if (isScale) {
+    rotate += 10;
+  }
+  ctx.translate(X_POS, Y_POS);
+  ctx.rotate((Math.PI / 180) * 10);
+  ctx.translate(-X_POS, -Y_POS);
+  render();
+}
+function reflectionHandler() {
+  ctx.translate(X_POS, Y_POS);
+  if (isScale) {
+    isRef = true;
+  }
+  ctx.scale(...ref);
+  ctx.translate(-X_POS, -Y_POS);
+  render();
+}
+function scaleHandler() {
+  if (isScale) {
+    scale = 1;
+    isScale = !isScale;
+    ctx.restore();
+    ctx.translate(X_POS, Y_POS);
+    ctx.scale(scale, scale);
+    ctx.rotate((Math.PI / 180) * rotate);
+    if (isRef) {
+      ctx.scale(...ref);
+      isRef = false;
+    }
+    rotate = 0;
+    ctx.translate(-X_POS, -Y_POS);
+    render();
+  } else {
+    scale = 1.3;
+    isScale = !isScale;
+    ctx.save();
+    ctx.translate(X_POS, Y_POS);
+    ctx.scale(scale, scale);
+    ctx.translate(-X_POS, -Y_POS);
+    render();
+  }
+}
+function draw() {
   ctx.fillStyle = "black";
 
-  // Draw black ellipse
   ctx.beginPath();
   ctx.ellipse(X_POS, Y_POS, 50, 55, 0, 0, 2 * Math.PI);
   ctx.fill();
 
   ctx.fillStyle = "white";
 
-  // Draw white ellipse
   ctx.beginPath();
   ctx.ellipse(X_POS, Y_POS, 35, 50, 0, 0, 2 * Math.PI);
   ctx.fill();
 
   ctx.fillStyle = "black";
 
-  // Draw first quadratic curve
   ctx.beginPath();
   ctx.moveTo(X_POS - 10, Y_POS + 50);
   ctx.quadraticCurveTo(X_POS - 10, Y_POS + 65, X_POS + 45, Y_POS + 76);
   ctx.fill();
 
-  // Draw second quadratic curve
   ctx.beginPath();
   ctx.moveTo(X_POS + 8, Y_POS + 50);
   ctx.quadraticCurveTo(X_POS - 20, Y_POS + 65, X_POS + 45, Y_POS + 75);
@@ -95,9 +116,25 @@ function render() {
 
   ctx.fillStyle = "white";
 
-  // Draw white quadratic curve
   ctx.beginPath();
   ctx.moveTo(X_POS + 15, Y_POS + 54);
   ctx.quadraticCurveTo(X_POS + 25, Y_POS + 71, X_POS + 47, Y_POS + 75);
   ctx.fill();
 }
+function render() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  draw();
+}
+function getMousePos(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top,
+  };
+}
+
+canvas.addEventListener("mousemove", mouseMoveHandler);
+canvas.addEventListener("contextmenu", contextMenuHandler);
+btnRotate.addEventListener("click", rotateHandler);
+btnReflection.addEventListener("click", reflectionHandler);
+btnScale.addEventListener("click", scaleHandler);
